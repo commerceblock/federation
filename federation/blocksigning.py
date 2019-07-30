e2d66485215243db2b834d36c8743b4d264841ea
nkostoulas <nkostoulas@gmail.com>
2019-07-30 19:14:30 +0300

    Separate inflation from blocksigning (Fixes #14)

--- federation/blocksigning.py
+++ federation/blocksigning.py
@@ -7,7 +7,7 @@ from .daemon import DaemonThread
 from .test_framework.authproxy import JSONRPCException
 from .messenger_factory import MessengerFactory
 from .connectivity import getoceand
-from decimal import Decimal
+from .inflation import Inflation
 
 BLOCK_TIME_DEFAULT = 60
 
@@ -23,31 +23,12 @@ class BlockSigning(DaemonThread):
 
         self.messenger = MessengerFactory.get_messenger(conf["msgtype"], nodes, self.my_id)
         self.signer = signer
-
-        self.rate = in_rate
-        self.period = in_period
-        self.address = in_address
-        self.script = script
-        self.inconf = 1
         self.nsigs = conf["nsigs"]
+
+        self.inflation = None
         if in_rate > 0:
-            try:
-                self.ocean.importprivkey(conf["reissuanceprivkey"],"privkey",True)
-            except Exception as e:
-                self.logger.error("{}\nFailed to import reissuance private key".format(e))
-                sys.exit(1)
-            self.riprivk = []
-            self.riprivk.append(conf["reissuanceprivkey"])
-            try:
-                p2sh = self.ocean.decodescript(script)
-            except Exception as e:
-                self.logger.error("{}\nFailed to decode reissuance script".format(e))
-                sys.exit(1)
-            self.p2sh = p2sh["p2sh"]
-            self.nsigs = p2sh["reqSigs"]
-            self.ocean.importaddress(self.p2sh,"reissuance")
-            validate = self.ocean.validateaddress(self.p2sh)
-            self.scriptpk = validate["scriptPubKey"]
+            self.inflation = Inflation(self.total, self.my_id, self.ocean,
+                in_rate, in_period, in_address, script, conf["reissuanceprivkey"])
 
     def run(self):
         while not self.stopped():
@@ -80,25 +61,12 @@ class BlockSigning(DaemonThread):
                     self.logger.warning("could not sign new block")
                     continue
 
-                # Inflation only check to see if there are any reissuance transactions to sign
-                if self.rate > 0 and height > 0:
-                    self.logger.info("node: {} - inflationconf: {}".format(str(self.my_id), str(self.inconf)))
-                    rtxs = []
-                    if height % self.period == 0:
-                        rtxs = self.get_reissuance_txs(height)
-                        self.inconf = 1 if len(rtxs) == 0 else 0
-                    if self.inconf == 0 and "txs" in new_block:
-                        sig["txsigs"] = None
+                # Inflation only, check to see if there are any reissuance transactions to sign
+                if height > 0 and self.inflation is not None:
+                    txsigs = self.inflation.get_tx_sigs(self.ocean, height, new_block)
+                    if txsigs is not None:
+                        sig["txsigs"] = txsigs
                         sig["id"] = self.my_id
-                        if height % self.period == 0:
-                            if rtxs == new_block["txs"]:
-                                sig["txsigs"] = self.get_tx_signatures(new_block["txs"], height, False)
-                            if sig["txsigs"] == None:
-                                self.logger.warning("could not sign reissuance txs on specified period block")
-                        elif height % self.period < self.period/2:
-                            sig["txsigs"] = self.get_tx_signatures(new_block["txs"], height, True)
-                            if sig["txsigs"] != None:
-                                self.logger.warning("reissuance txs signed with delay of "+str(height % self.period)+" blocks")
 
                 self.messenger.produce_sig(sig, height + 1)
                 elapsed_time = time() - start_time
@@ -115,37 +83,13 @@ class BlockSigning(DaemonThread):
                     continue
 
                 #if reissuance step, create raw reissuance transactions
-                do_submit = 0
-                if self.rate > 0 and height > 0:
-                    self.logger.info("node: {} - inflationconf: {}".format(str(self.my_id), str(self.inconf)))
-                    if height % self.period == 0:
-                        block["txs"] = self.get_reissuance_txs(height)
-                        if block["txs"] == None:
-                            self.logger.warning("could not create reissuance txs")
-                            continue
-                        if len(block["txs"]) == 0:
-                            self.logger.info("no issued assets to inflate")
-                            self.inconf = 1
-                        else:
-                            self.inconf = 0
-                            do_submit = 1
-                    #if not reissuance step, in first 1/2 of inflation period and issuance not confirmed, then verify issuance
-                    elif self.inconf == 0 and height % self.period > 2 and height % self.period < self.period/2:
-                        riconf = self.confirm_reissuance_txs(height)
-                        self.logger.info("confirmed: "+str(riconf)+" node "+str(self.my_id))
-                        if riconf:
-                            self.inconf = 1
-                        else:
-                            #attempt reissuance again
-                            block["txs"] = self.get_reissuance_txs(height)
-                            self.inconf = 0
-                            do_submit = 1
-                            if block["txs"] == None:
-                                self.logger.warning("could not create reissuance txs on retry")
-                                continue
-                    #if reissuance still not confirmed after 30 minutes (period/2) then stop
-                    elif self.inconf == 0 and height % self.period > self.period/2:
-                        self.logger.error("FATAL: could not issue inflation transactions")
+                if height > 0 and self.inflation is not None:
+                    try:
+                        txs = self.inflation.create_txs(self.ocean, height)
+                        if txs is not None:
+                            block["txs"] = txs
+                    except Exception as e:
+                        self.logger.error(e)
                         self.stop()
 
                 self.messenger.produce_block(block, height + 1)
@@ -158,20 +102,11 @@ class BlockSigning(DaemonThread):
                     self.logger.warning("could not get new block sigs")
                     self.messenger.reconnect()
                     continue
-                if self.rate > 0:
-                    if do_submit == 1:
-                        txsigs = [None] * self.total
-                        for sig in sigs:
-                            if "id" in sig and "txsigs" in sig:
-                                txsigs[sig["id"]] = sig["txsigs"]
-                        #add sigs for this node
-                        mysigs = self.get_tx_signatures(block["txs"], height, False)
-                        txsigs[self.my_id] = mysigs
-                        signed_txs = self.combine_tx_signatures(block["txs"],txsigs)
-                        sent = self.send_reissuance_txs(signed_txs)
-                        if sent == None:
-                            self.logger.warning("could not send reissuance transactions. node "+str(self.my_id))
-                            continue
+
+                if self.inflation is not None and "txs" in block and block["txs"] is not None:
+                    if not self.inflation.send_txs(self.ocean, height, block, sigs):
+                        continue
+
                 blocksigs = []
                 for sig in sigs:
                     blocksigs.append(sig["blocksig"])
@@ -224,179 +159,6 @@ class BlockSigning(DaemonThread):
                 self.logger.info("node {} - block not submitted".format(self.my_id))
         except Exception as e:
             self.logger.warning("{}\ncould not generate signed block".format(e))
-
-    def get_reissuance_txs(self, height):
-        #check that the reissuance address has been imported, and if not import
-        if self.ocean.getaccount(self.p2sh) != "reissuance":
-            self.ocean.importaddress(self.p2sh,"reissuance")
-        try:
-            token_addr = self.p2sh
-            raw_transactions = []
-            #retrieve the token report for re-issuing
-            utxorep = self.ocean.getutxoassetinfo()
-            #get the reissuance tokens from wallet
-            unspentlist = self.ocean.listunspent()
-            frzhist = self.ocean.getfreezehistory() # get freeze history
-            for unspent in unspentlist:
-                #re-issuance and policy tokens have issued amount of 10000 as a convention
-                if "address" in unspent:
-                    if unspent["address"] == token_addr and unspent["amount"] > 9999.0:
-                        #find the reissuance details and amounts
-                        for entry in utxorep:
-                            if entry["token"] == unspent["asset"]:
-                                amount_spendable = float(entry["amountspendable"])
-                                amount_frozen = float(entry["amountfrozen"])
-                                asset = entry["asset"]
-                                entropy = entry["entropy"]
-                                break
-                        #the spendable amount needs to be inflated over a period of 1 hour
-                        total_reissue = amount_spendable*(1.0+float(self.rate))**(1.0/(24*365))-amount_spendable
-                        #check to see if there are any assets unfrozenx in the last interval
-                        amount_unfrozen = 0.0
-                        for frzout in frzhist:
-                            if frzout["asset"] == asset:
-                                if frzout["end"] != 0 and frzout["end"] > height - self.period:
-                                    backdate = height - frzout["start"]
-                                    elapsed_interval = backdate // self.period
-                                    self.logger.info("elapsed_interval: "+str(elapsed_interval))
-                                    amount_unfrozen = float(frzout["value"])
-                                    total_reissue += amount_unfrozen*(1.0+float(self.rate))**(elapsed_interval/(24*365))-amount_unfrozen
-                                    self.logger.info("backdate reissue: "+ str(total_reissue))
-                        self.logger.info("Reissue asset "+asset+" by "+str(round(total_reissue,8)))
-                        tx = self.ocean.createrawreissuance(self.address,str("%.8f" % round(total_reissue,8)),token_addr,str(unspent["amount"]),unspent["txid"],str(unspent["vout"]),entropy)
-                        tx["token"] = unspent["asset"]
-                        tx["txid"] = unspent["txid"]
-                        tx["vout"] = unspent["vout"]
-                        raw_transactions.append(tx)
-            return raw_transactions
-        except Exception as e:
-            self.logger.warning("failed reissuance tx generation: {}".format(e))
-            return None
-
-    def confirm_reissuance_txs(self, height):
-        try:
-            token_addr = self.p2sh
-            utxorep = self.ocean.getutxoassetinfo()
-            #get the reissuance tokens from wallet
-            unspentlist = self.ocean.listunspent()
-            numissue = 0
-            numnew = 0
-            for unspent in unspentlist:
-                #re-issuance and policy tokens have issued amount of 10000 as a convention
-                if "address" in unspent:
-                    if unspent["address"] == token_addr and unspent["amount"] > 9999.0:
-                        numissue += 1
-                        #check confirmation time - all reissuance tokens should have confirmed in the last 20 blocks (period/3)
-                        if unspent["confirmations"] > self.period/3:
-                            self.logger.warning("Warning: reissuance failure - unspent reissuance token output: ")
-                            self.logger.warning(unspent)
-                        else:
-                            numnew += 1
-            #if no reissuances or all issuances confirmed, return true
-            if numissue == 0:
-                return True
-            elif numnew == numissue:
-                return True
-            #if there are reissuances, but they are not confirmed, return false
-            elif numnew == 0 and numissue > 0:
-                return False
-            #if only some reissuances have confirmed but other haven't - stop
-            else:
-                self.logger.error("FATAL: inflation transactions partially confirmed")
-                self.stop()
-        except Exception as e:
-            self.logger.warning("failed confirmation check: {}".format(e))
-            return False
-
-    def check_reissuance(self, transactions, height):
-        try:
-            mytransactions = self.get_reissuance_txs(height)
-            if mytransactions == transactions:
-                return True
-            else:
-                return False
-        except Exception as e:
-            self.logger.warning("failed reissuance tx checking: {}".format(e))
-            return False
-
-    def get_tx_signatures(self, transactions, height, check):
-        try:
-            signatures = []
-            if not check or self.check_reissuance(transactions, height):
-                for tx in transactions:
-                    inpts = []
-                    inpt = {}
-                    inpt["txid"] = tx["txid"]
-                    inpt["vout"] = tx["vout"]
-                    inpt["scriptPubKey"] = self.scriptpk
-                    inpt["redeemScript"] = self.script
-                    inpts.append(inpt)
-                    signedtx = self.ocean.signrawtransaction(tx["hex"],inpts,self.riprivk)
-                    sig = ""
-                    scriptsig = signedtx["errors"][0]["scriptSig"]
-                    ln = int(scriptsig[2:4],16)
-                    if ln > 0: sig = scriptsig[2:4] + scriptsig[4:4+2*ln]
-                    signatures.append(sig)
-            else:
-                self.logger.warning("reissuance tx error, node: {}".format(self.my_id))
-            return signatures
-        except Exception as e:
-            self.logger.warning("failed reissuance tx signing: {}".format(e))
-            return None
-
-    def int_to_pushdata(self,x):
-        x = int(x)
-        if x < 253:
-            return "{:02x}".format(x)
-        else:
-            le = "{:04x}".format(x)
-            be = "".join([le[x:x+2] for x in range(0,len(le),2)][::-1])
-            return "fd"+be
-
-    def combine_tx_signatures(self, transactions, signatures):
-        try:
-            itr_tx = 0
-            for tx in transactions:
-                mtx_p = tx["hex"][0:84]
-                mtx_s = tx["hex"][86:]
-                sigs = []
-                #for each tx, get the signatures
-                for itr in range(len(signatures)):
-                    try:
-                        sig = signatures[itr][itr_tx]
-                        sigs.append(sig)
-                        if len(sigs) == self.nsigs: break
-                    except:
-                        self.logger.warning("missing node {} signatures".format(itr))
-                scriptsig = "00"
-                if len(sigs) != self.nsigs:
-                    self.logger.warning("error: insufficient sigs for tx {}".format(itr_tx))
-                else:
-                    #concatenate sigs
-                    for s in sigs:
-                        scriptsig += s
-                    #add the redeem script
-                    rsln = len(self.script)//2
-                    lnh = hex(rsln)
-                    scriptsig += "4c" + lnh[2:] + self.script
-                sslh = self.int_to_pushdata(len(scriptsig)//2)
-                tx["hex"] = mtx_p + sslh + scriptsig + mtx_s
-                itr_tx += 1
-            return transactions
-        except Exception as e:
-            self.logger.warning("failed signature combination: {}".format(e))
-            return None
-
-    def send_reissuance_txs(self, transactions):
-        txids = []
-        try:
-            for tx in transactions:
-                txid = self.ocean.sendrawtransaction(tx["hex"])
-                txids.append(txid)
-            return txids
-        except Exception as e:
-            self.logger.warning("failed tx sending: {}".format(e))
-            return None
 
 OCEAN_BASE_HEADER_SIZE = 172
 
